5ad69a1474544bb27d471bbdc8f0e20c6644e0b6
Emir Salih <137483300+EmirSlh@users.noreply.github.com>
2024-05-28 15:55:39 +0300

    script.js

--- script.js
+++ script.js
@@ -5,26 +5,22 @@ window.addEventListener('scroll', function() {
       const elementTop = element.getBoundingClientRect().top;
       const windowHeight = window.innerHeight;
   
-      if (elementTop < windowHeight * 0.8) { // Ekranın %80'ine gelince
-        element.classList.add('visible'); // 'visible' sınıfını ekle
+      if (elementTop < windowHeight * 0.8) { 
+        element.classList.add('visible'); 
       } 
-      // else { 
-      //   element.classList.remove('visible'); // (İsteğe bağlı) Yukarı kaydırınca efekti geri almak için
-      // }
+      
     });
   });
 
   const menuLinks = document.querySelectorAll('#menu a');
 
-// Her bağlantı için tıklama olayını dinliyoruz
 menuLinks.forEach(link => {
   link.addEventListener('click', function(event) {
-    event.preventDefault(); // Varsayılan bağlantı davranışını engelle
+    event.preventDefault(); 
 
-    const targetId = this.getAttribute('href'); // Bağlantının href değerini al (örneğin, "#hakkımızda")
-    const targetElement = document.querySelector(targetId); // İlgili öğeyi seçiyoruz
+    const targetId = this.getAttribute('href'); 
+    const targetElement = document.querySelector(targetId); 
 
-    // Yumuşak geçiş ile hedefe ilerliyoruz
     targetElement.scrollIntoView({
       behavior: 'smooth' 
     });
